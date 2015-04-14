e86ba075f05d369d4e30df6368af17a71af7bf34
erpreciso <trepreciso@gmail.com>
2015-04-14 11:09:21 +0200

    Use median substitution for NAs

--- titanic-script.R
+++ titanic-script.R
@@ -7,23 +7,10 @@ train <- read.csv("data/train.csv")
 test <- read.csv("data/test.csv")
 
 ## simple prevision based on sex and age
-# proportion table for sex
-prop.table(table(train$Sex, train$Survived), 1)
-# table for sex and ages in categories
-train$AgeSimulated <- na.approx(train$Age) # simulate NA in Age in train
-train$AgeCluster <- cut(train$AgeSimulated, seq(0,80,5))
-# aggregate(Survived ~ Sex + AgeCluster, data=train, FUN=sum)
-qplot(AgeCluster,   # visualize
-      data=train,
-      fill=as.factor(Survived),
-      facets= . ~ Sex,
-      geom="histogram")
-
-
-simple.model <- lm(Survived ~ Sex + AgeSimulated, data=train)
+train[is.na(train$Age), "Age"] <- median(train$Age, na.rm=TRUE)
+simple.model <- lm(Survived ~ Sex + Age, data=train)
+test[is.na(test$Age), "Age"] <- median(test$Age, na.rm=TRUE)
 res <- data.frame(Sex=test$Sex, Age=test$Age, PassengerId=test$PassengerId)
-res$AgeSimulated <- na.approx(res$Age, na.rm=FALSE) # simulate NAs
-res$AgeSimulated <- na.locf(res$AgeSimulated) # simulate trailing NAs
 res$Survived <- round(predict(simple.model, res))
 res <- res[,c("PassengerId", "Survived")]
 # check of no missing values
